f218349f2000af8250d03fc535ae257aab62cc2c
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-27 18:26:09 +0500

    task20_rekursia_massiva_matrix

---
+++ task20/Program.cs
@@ -0,0 +1,10 @@
+﻿// Двумерные массивы
+int[,] matrix =new int [3,4];
+for (int i = 0; i < 3; i++)
+{
+    for(int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
