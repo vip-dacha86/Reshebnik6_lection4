582dd19bc3763012753d5db2d7e8b2f4d5e55e44
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-27 18:05:07 +0500

    task19_rekursia_massiva

---
+++ task19/Program.cs
@@ -0,0 +1,13 @@
+﻿// // Двумерные массивы
+string [,] table = new string[2, 5];
+// string.Empty
+// table[0,0] table[0,1]table[0,2]table[0,4]
+// table[1,0] table[1,1]table[1,2]table[1,4]
+table[1, 2] = "Слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0;columns < 5;columns++)
+{
+    Console.WriteLine($" -{table[rows,columns]}- ");
+}
+}
